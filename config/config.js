2bcae673cac2372b30e2207ada23d0fbf44957f0
Gloire__ODG <93401301+ondongo@users.noreply.github.com>
2024-12-10 21:32:20 +0100

    (fix): fix seed slot

--- config/config.js
+++ config/config.js
@@ -1,12 +1,5 @@
 require('dotenv').config();
 
-console.log("Environment Variables:");
-console.log("DB_USER:", process.env.DB_USER);
-console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
-console.log("DB_NAME:", process.env.DB_NAME);
-console.log("DB_HOST:", process.env.DB_HOST);
-console.log("DB_PORT:", process.env.DB_PORT);
-
 module.exports = {
   development: {
     username: process.env.DB_USER,
