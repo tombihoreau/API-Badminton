33874538985cdb5a8530656498474f3ea35f10c9
Gloire__ODG <93401301+ondongo@users.noreply.github.com>
2024-12-10 20:05:12 +0100

    (fix) : Reorder migrations: create-fields, create-slots, create-reservations

--- config/config.js
+++ config/config.js
@@ -12,7 +12,7 @@ module.exports = {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    host: "localhost" || process.env.DB_HOST,
+    host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: "mysql"
   },
