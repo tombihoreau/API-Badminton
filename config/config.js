d8474d28c9e38f3792f4a4d1408930d3ed7fb3d9
Gloire__ODG <93401301+ondongo@users.noreply.github.com>
2024-12-10 19:45:50 +0100

    (fix) : fix attributes types and config

--- config/config.js
+++ config/config.js
@@ -1,18 +1,27 @@
 require('dotenv').config();
 
+console.log("Environment Variables:");
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
+console.log("DB_NAME:", process.env.DB_NAME);
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_PORT:", process.env.DB_PORT);
+
 module.exports = {
   development: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    host: "localhost" || process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: "mysql"
   },
   test: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    host: process.env.DB_HOST ,
+    port: process.env.DB_PORT,
     dialect: "mysql"
   },
   production: {
@@ -20,6 +29,8 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: "mysql"
   }
 };
+
--- seeders/20241210175739-create-users.js
+++ seeders/20241210175739-create-users.js
@@ -1,25 +1,35 @@
-'use strict';
+"use strict";
+const bcrypt = require("bcryptjs");
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface, Sequelize) {
+    const adminPassword = bcrypt.hashSync("astrongpassword", 10);
+    const playerPassword = bcrypt.hashSync("motdepasseplayer", 10);
+
+    await queryInterface.bulkInsert(
+      "Users",
+      [
+        {
+          pseudo: "admybad",
+          password: adminPassword,
+          role: "admin",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          pseudo: "player1",
+          password: playerPassword, 
+          role: "user",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("Users", null, {});
+  },
 };
